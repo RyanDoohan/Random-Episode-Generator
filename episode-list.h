c0f7f40a0922fa23ab1e65d88cdf0cdaab19c95d
Ryan <doohanryan@gmail.com>
2022-03-18 23:23:41 -0400

    Added comments for input file parser

--- episode-list.h
+++ episode-list.h
@@ -5,19 +5,19 @@
 #include <fstream>
 #include <vector>
 
-struct Episode {
+struct Episode { // Holds the episodes season, number, and name.
             int episodeNumber, episodeSeason;
             std::string episodeName;
         };
 
 class EpisodeList {
     private:
-        int numEpisodes;
-        std::vector<Episode> EpisodeLst;
+        int numEpisodes; // Number of episodes currently stored in the episode list.
+        std::vector<Episode> EpisodeLst; // Vector of Episode structs. Holds all epidoes.
     public:
-        EpisodeList() {}
-        
-        EpisodeList(int episodeSeas, int episodeNum, std::string name) {
+        EpisodeList() {} // Default constructor with no arguments.
+
+        EpisodeList(int episodeSeas, int episodeNum, std::string name) { // Default constructor for adding first episode.
             Episode episode;
             episode.episodeNumber = episodeNum;
             episode.episodeSeason = episodeSeas;
@@ -26,7 +26,7 @@ class EpisodeList {
             numEpisodes = 1;
         }
 
-        Episode getEpisode(int index) {
+        Episode getEpisode(int index) { // Return an episode at a given index in EpisodeLst.
             Episode episode;
 
             episode.episodeName = EpisodeLst[index].episodeName;
@@ -36,16 +36,16 @@ class EpisodeList {
             return episode;
         }
 
-        void addEpisode(Episode newEpisode) {
+        void addEpisode(Episode newEpisode) { // Push a new episode to EpisodeLst.
             EpisodeLst.push_back(newEpisode);
-            numEpisodes++;
+            numEpisodes++; // Increment number of episodes.
         }
 
-        int getNumEpisodes() {
+        int getNumEpisodes() { // Get number of episodes.
             return numEpisodes;
         }
 
-        std::string getEpisodeString(int index) {
+        std::string getEpisodeString(int index) { // Will return a single formatted episode string for output.
             std::string result;
             
             result = std::to_string(EpisodeLst[index].episodeSeason) + " ";
@@ -55,19 +55,19 @@ class EpisodeList {
             return result;
         }
 
-        void displayAllEpisodes() {
+        void displayAllEpisodes() { // Iterate through the whole EpisodeLst vector and display all episodes.
             for(int i = 0; i < EpisodeLst.size(); i++) {
                 std::cout << getEpisodeString(i) << std::endl;
             }
         }
 
-        void displayEpisodeNames() {
+        void displayEpisodeNames() { // Iterate through the whole EpisodeLst vector and display all episode names.
             for(Episode autoEpis : EpisodeLst) {
                 std::cout << autoEpis.episodeName << std::endl;
             }
         }
 
-        void displayEpisode(Episode epis) {
+        void displayEpisode(Episode epis) { // Display a single formatted episode string, given an episode.
             std::string output;
 
             output = std::to_string(epis.episodeSeason) + " ";
@@ -77,31 +77,31 @@ class EpisodeList {
             std::cout << output << std::endl;
         }
 
-        void inputEpisodes(std::string fileName) {
-            std::ifstream episodeFile;
-            Episode epis;
+        void inputEpisodes(std::string fileName) {  // Episode file parser that uses 's:' as a delimiter for splitting up seasons
+            Episode epis;                           // each new episode is separated by a newline char. The parser will automatically look for this format
+                                                    // every delimiter within the episode input file will indicate a new season.
+            std::ifstream episodeFile;              // The file parser looks for files only within the same host directory.
+            std::string fileLine;
             int episNum = 0, episSeas = 0;
 
             episodeFile.open(fileName + ".txt");
-
-            std::string fileLine;
-            if(episodeFile.is_open()) {
+            if(episodeFile.is_open()) { // Verify the input file was opened succesfully.
                 while(std::getline(episodeFile, fileLine)) {
-                    if(fileLine == "s:") {
+                    if(fileLine == "s:") { // Check to see if fileLine contains the new season delimiter.
                         episSeas++;
-                        episNum = 0;
+                        episNum = 0; // Reset episode count.
                     }
                     else {
                         episNum++;
                         epis.episodeSeason = episSeas;
                         epis.episodeNumber = episNum;
                         epis.episodeName = fileLine;
-                        addEpisode(epis);
+                        addEpisode(epis); // Push new episode to EpisodeLst.
                     }
                 }
             }
             else {
-                std::cout << "Invalid file name!\n";
+                std::cout << "Invalid file name!\n"; // Error handling for invalid file.
             }
         }
 };
