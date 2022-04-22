f4a58273dc1679c0c618311cf14735f3d37ebcd8
Ryan <doohanryan@gmail.com>
2022-04-22 14:36:00 -0400

    Added display function for show list.

--- shows-list.h
+++ shows-list.h
@@ -5,12 +5,28 @@
 #include <vector>
 #include "episode-list.h"
 
+struct Show {
+    std::string showName;
+    EpisodeList showEpisodes;
+};
+
 class ShowList {
     private:
-        std::vector<EpisodeList> ShowLst;
+        std::vector<Show> showLst;
 
     public:
         ShowList() {} // Default constructor with no arguments.
+
+        displayShowsByName() {
+            for(int i = 0; i < showLst.size(); i++) {
+                std::cout << showLst[i].showName;
+            }
+        }
+
+        insertNewShow(std::string name) {
+            Show newShow;
+            newShow.showName = name;
+        }
 };
 
 #endif
