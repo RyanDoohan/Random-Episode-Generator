007568abfbc6656e1425344d1aacf91e5ec043dd
Ryan <doohanryan@gmail.com>
2022-04-21 21:13:21 -0400

    Added additional display function for main menu options

--- display-functions.h
+++ display-functions.h
@@ -4,9 +4,9 @@
 #include <iostream>
 #include "episode-list.h"
 
-bool firstRunFlag = true;
+bool ensureDisplayFirstRun = true, ensureEpisodesAdded = true;
 
-void displayWelcomeMessage(std::string & inputFile) {
+void getEpisodeFile(std::string & inputFile) {
     std::cout << "\n\nEnter the name of the input file, this file must be in the same host directory!\n : ";
     std::getline(std::cin, inputFile);
 }
@@ -22,18 +22,18 @@ EpisodeList inputEpisodeFile(std::string inputEpisodeFile) {
     }
 }
 
-int displayMenu() {
+int displayChoiceMenu() {
     std::string userChoice;
 
-    if(firstRunFlag == true) {
+    if(ensureDisplayFirstRun == true) {
         std::cout << "Welcome to the random episode generator!";
-        firstRunFlag = false;
+        ensureDisplayFirstRun = false;
     }
 
-    std::cout << "\n\n1.) Generate a random episode.\n";
+    std::cout << "\n\n1.) Import a new episode file.\n";
     std::cout << "2.) Display all episode entries with season & episode number.\n";
     std::cout << "3.) Display all episode name entries.\n";
-    std::cout << "4.) Import a new episode file.\n";
+    std::cout << "4.) Generate a random episode.\n";
     std::cout << "5.) Exit.\n\n";
     std::cout << "Enter choice: ";
 
@@ -54,6 +54,58 @@ void generateRandomEpisode(EpisodeList episList) {
 
         std::cout << "\n\nWould you like another episode? (y/n): ";
         std::getline(std::cin, stop);
+    }
+}
+
+void displayMainMenu() {
+    EpisodeList episList;
+    std::string inputFile, stop = "y";
+
+    while(stop == "y") {
+        int retVal = displayChoiceMenu();
+
+        switch(retVal) {
+            case 1: {
+                getEpisodeFile(inputFile);
+                episList = inputEpisodeFile(inputFile);
+                ensureEpisodesAdded = false;
+                break;
+            }
+            case 2: {
+                if(ensureEpisodesAdded == false) {
+                    episList.displayAllEpisodes();
+                }
+                else {
+                    std::cout << "\n*Error: Must import an episode file first!";
+                }
+                break;
+            }
+            case 3: {
+                if(ensureEpisodesAdded == false) {
+                    episList.displayEpisodeNames();
+                }
+                else {
+                    std::cout << "\n*Error: Must import an episode file first!";
+                }
+                break;
+            }
+            case 4: {
+                if(ensureEpisodesAdded == false) {
+                    generateRandomEpisode(episList);
+                }
+                else {
+                    std::cout << "\n*Error: Must import an episode file first!";
+                }
+                break;
+            }
+            case 5: {
+                stop = "n"; // Update the stop string to terminate.
+                break;
+            }
+            default: {
+                std::cout << "\n*Error: Invalid input has been entered. Please restart!";
+            }
+        }
     }
 }
 
--- episode-generator.cpp
+++ episode-generator.cpp
@@ -4,39 +4,7 @@
 #include "display-functions.h"
 
 int main() {
-    EpisodeList episList;
-    std::string inputFile;
-    int retVal;
-
-    std::string stop = "y";
-
-    while(stop == "y") {
-    retVal = displayMenu();
-
-    switch(retVal) {
-        case 1: {
-            generateRandomEpisode(episList);
-            break;
-        }
-        case 2: {
-            episList.displayAllEpisodes();
-            break;
-        }
-        case 3: {
-            episList.displayEpisodeNames();
-            break;
-        }
-        case 4: {
-            displayWelcomeMessage(inputFile);
-            episList = inputEpisodeFile(inputFile);
-            break;
-        }
-        case 5: {
-            stop = "n";
-        }
-        default: {}
-    }
-    }
+    displayMainMenu();
     
     return 0;
 }
